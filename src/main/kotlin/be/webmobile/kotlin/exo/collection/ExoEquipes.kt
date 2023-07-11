f21e3034af108ad9c491f19a22274f3f6286834e
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2023-07-11 16:13:58 +0200

    [correction]: collections - equipe: gestion exc. + comment.

--- src/main/kotlin/be/webmobile/kotlin/exo/collection/ExoEquipes.kt
+++ src/main/kotlin/be/webmobile/kotlin/exo/collection/ExoEquipes.kt
@@ -1,11 +1,13 @@
 package be.webmobile.kotlin.exo.collection
 
+import java.lang.NullPointerException
+
 fun main() {
 
     val joueursSansEquipe: MutableList<String> = mutableListOf()
     val equipes: MutableMap<String, MutableList<String>> = mutableMapOf()
 
-    var input: Int;
+    var input: Int = 1;
 
     do {
         println("""
@@ -16,70 +18,78 @@ fun main() {
             4 - afficher membre d'une equipe
             5 - quitter
         """.trimIndent())
+        try {
+            input = readln().toInt()
+            when (input) {
+                1 -> {
+                    println("Quel nom?")
+                    joueursSansEquipe.add(readln())
+                }
 
-        input = readln().toInt()
-
-        when(input) {
-            1 -> {
-                println("Quel nom?")
-                joueursSansEquipe.add( readln() )
-            }
-            2 -> {
-                println("quel nom?")
-                val nom = readln()
-                if( nom !in equipes.keys )
-                    equipes[nom] = mutableListOf()
+                2 -> {
+                    println("quel nom?")
+                    val nom = readln()
+                    if (nom !in equipes.keys)
+                        equipes[nom] = mutableListOf()
 //                    equipes.put( nom, mutableListOf() ) // pareil
-                else
-                    println("Cette equipe existe déjà")
-            }
-            3 -> {
-                println("Nom de l'equipe?")
-                val nomEquipe = readln()
-                if( nomEquipe in equipes.keys ){
-                    println("Nom du joueur?")
-                    val nomJoueur = readln()
-                    if( nomJoueur in joueursSansEquipe ) {
-                        joueursSansEquipe.remove(nomJoueur)
-                        equipes[nomEquipe]!!.add(nomJoueur)
-                        println("Le joueur $nomJoueur a été ajouté à $nomEquipe")
+                    else
+                        println("Cette equipe existe déjà")
+                }
+
+                3 -> {
+                    println("Nom de l'equipe?")
+                    val nomEquipe = readln()
+                    if (nomEquipe in equipes.keys) {
+                        println("Nom du joueur?")
+                        val nomJoueur = readln()
+                        if (nomJoueur in joueursSansEquipe) {
+                            joueursSansEquipe.remove(nomJoueur)
+                            equipes[nomEquipe]!!.add(nomJoueur)
+                            println("Le joueur $nomJoueur a été ajouté à $nomEquipe")
+                        } else {
+                            println("Le joueur n'existe pas ou a déjà intégré une équipe")
+                        }
                     } else {
-                        println("Le joueur n'existe pas ou a déjà intégré une équipe")
+                        println("L'équipe n'existe pas")
                     }
                 }
-                else {
-                    println("L'équipe n'existe pas")
-                }
-            }
-            4 -> {
-                println("Quelle equipe?")
-                val nomEquipe = readln()
-                if( nomEquipe in equipes.keys ){
-                    println("-- Equipe $nomEquipe --")
 
-                    // Facon 1
+                4 -> {
+                    println("Quelle equipe?")
+                    val nomEquipe = readln()
+                    if (nomEquipe in equipes.keys) {
+                        println("-- Equipe $nomEquipe --")
+
+                        // Facon 1
 //                    val equipe = equipes[nomEquipe]
 //                    if( equipe != null ){
 //                        for (s in equipe) {
 //                            println("- $s")
 //                        }
 //                    }
-                    // facon 2
+                        // facon 2
 //                    for (membre in equipes[nomEquipe]!!){ // raccourci de la méthode .get(key)
-                    // facon 3
+                        // facon 3
 //                    for (membre in equipes.get(nomEquipe)!!){
-                    // facon 4
-                    for (membre in equipes.getValue(nomEquipe)){
-                        println("- $membre")
+                        // facon 4
+                        for (membre in equipes.getValue(nomEquipe)) {
+                            println("- $membre")
+                        }
+                    } else {
+                        println("L'equipe n'existe pas")
                     }
                 }
-                else {
-                    println("L'equipe n'existe pas")
-                }
+
+                5 -> println("au revoir")
+                else -> println("commande invalide")
             }
-            5 -> println("au revoir")
-            else -> println("commande invalide")
         }
+        catch ( e: NumberFormatException ){
+            println("commande invalide")
+        }
+//        catch ( e: NullPointerException ){ // les !! utilisés sont tous utilisé de facon "safe". On verifie au préalable qu'ils ne posent pas de problème
+//            println("on ne passe jamais par ici")
+//        }
 
     } while ( input != 5 );
 
